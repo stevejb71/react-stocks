134d42b0ef8868aa5c32c0ed28cc7c327aa7bdf2
steve <stevejb71@gmail.com>
2018-07-07 21:41:19 +0800

    Better error checking

--- src/Alphavantage.js
+++ src/Alphavantage.js
@@ -2,18 +2,31 @@
 
 import axios from 'axios'
 
+const hasKey = (data: any, key: string): bool => {
+  try {
+    // eslint-disable-next-line
+    return data[key] !== undefined;
+  } catch (e) {
+    return false
+  }
+}
+
 export const getTimeSeries = (apiKey: string, symbol: string, dataHandler: number => void, onError: (number, string) => void) => {
   const handler = (response) => {
     if(response.status !== 200) {
       onError(response.status, response.statusText)
     } else {
-      dataHandler(response)
+      if(typeof response !== "object" || !response.hasOwnProperty('data') || !hasKey(response.data, 'Time Series (1min)')) {
+        onError(response.status, `Corrupt response for ${symbol}`)
+      } else {
+        dataHandler(response)
+      }
     }
   }
   axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=1min&apikey=${apiKey}`).then(handler)
 }
 
-export const latestClose = (handler: number => void) => (response: any) => {
+export const latestClose = (handler: number => void) => (response: any) => {  
   const timeSeries = Object.entries(response.data['Time Series (1min)']);
   const latestEntry = timeSeries[timeSeries.length - 1];
   const latestValue: any = latestEntry[1];
