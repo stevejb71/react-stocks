b5f7a0ca287c8f70e84ee79c8d4b0d6c3e911d78
steve <stevejb71@gmail.com>
2018-07-07 16:55:23 +0800

    Add flow

--- src/Alphavantage.js
+++ src/Alphavantage.js
@@ -1,6 +1,8 @@
+// @flow
+
 import axios from 'axios'
 
-export const getTimeSeries = (apiKey, symbol, dataHandler, onError) => {
+export const getTimeSeries = (apiKey: string, symbol: string, dataHandler: number => void, onError: (number, string) => void) => {
   const handler = (response) => {
     if(response.status !== 200) {
       onError(response.status, response.statusText)
@@ -11,8 +13,9 @@ export const getTimeSeries = (apiKey, symbol, dataHandler, onError) => {
   axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=1min&apikey=${apiKey}`).then(handler)
 }
 
-export const latestClose = (handler) => (response) => {
+export const latestClose = (handler: number => void) => (response: any) => {
   const timeSeries = Object.entries(response.data['Time Series (1min)']);
   const latestEntry = timeSeries[timeSeries.length - 1];
-  handler(Number(latestEntry[1]["4. close"]));
+  const latestValue: any = latestEntry[1];
+  handler(Number(latestValue["4. close"]));
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { getTimeSeries, latestClose } from  './Alphavantage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const root = document.getElementById('root')
+if(root != null)
+{
+  ReactDOM.render(<App />, root);
+  registerServiceWorker();
+}
+
+const handler = latestClose(console.log);
+getTimeSeries("DEMO", "MSFT", handler, (s, msg) => {})
