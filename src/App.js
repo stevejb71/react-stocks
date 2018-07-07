cba5d05c16a7f4643b6f52f3c92963c954093607
steve <stevejb71@gmail.com>
2018-07-07 18:09:33 +0800

    Add state for close price, and get it from AlphaVantage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+// @flow
+
 import React, { Component } from 'react';
 import './App.css';
+import { getTimeSeries, latestClose } from  './Alphavantage';
 
-class App extends Component {
+class App extends Component<any, any> {
   render() {
     return (
       <div className="Stocks">
@@ -12,13 +15,32 @@ class App extends Component {
   }
 }
 
-class Stock extends Component {
+type StockProps = {
+  symbol: string,
+  name: string
+}
+
+type StockState = {
+  price: number,
+}
+
+class Stock extends Component<StockProps, StockState> {
+  constructor(props) {
+    super(props);
+    this.state = {price: 0.0};
+  }
+
+  componentDidMount() {
+    const handler = latestClose(closePrice => this.setState({price: closePrice}));
+    getTimeSeries("DEMO", "MSFT", handler, (s, msg) => {})
+  }
+  
   render() {
     return (
       <div className="StockRow">
         <div className="StockCell">{this.props.symbol}</div>
         <div className="StockCell">{this.props.name}</div>
-        <div className="StockCell">{this.props.price}</div>
+        <div className="StockCell">{this.state.price}</div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { getTimeSeries, latestClose } from  './Alphavantage';
 
 const root = document.getElementById('root')
 if(root != null)
@@ -13,6 +12,3 @@ if(root != null)
   ReactDOM.render(<App />, root);
   registerServiceWorker();
 }
-
-const handler = latestClose(console.log);
-getTimeSeries("DEMO", "MSFT", handler, (s, msg) => {})
